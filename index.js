32961368d8319001f26ea23246c766d671f47d4a
Jakob Skov Olsen <jso@yu.agency>
2021-07-02 16:14:09 +0200

    initial eslint settings

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'indent': [
+      'warn',
+      2,
+    ],
+    'quotes': [
+      'warn',
+      'single',
+    ],
+    'no-console': [
+      'warn',
+      {
+        'allow': [
+          'warn',
+          'error',
+        ],
+      },
+    ],
+    'comma-dangle': [
+      'error', 'always-multiline',
+    ],
+  },
+}
