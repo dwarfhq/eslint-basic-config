77533086aa9e313f26371cb4f4cfc621a1ef908d
Jakob Skov Olsen <jsk@dwarf.dk>
2023-06-16 13:52:35 +0200

    Bumped ecma version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2022, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
